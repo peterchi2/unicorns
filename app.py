fb906637e9092adc4bc4f21da6366c9c4472070c
Unicorn Rentals <elasticvolcano@gmail.com>
2020-08-26 14:31:56 +0000

    Nothing to see here, move along

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = '83ee9c0da36408db0cdd70f72d782042'
-__version__ = 'e51e35a9cc5e2b4d2c84eb7dc1404cb3'
+__vcs_id__ = '8478125900ab2c15cd2cddf75ba5c294'
+__version__ = 'a5de41d5fc7ab4dcb9232a49318892f6'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
